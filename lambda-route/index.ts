b7014e2a4ede892b83565377d9cba4718d99b48f
pauls0 <pauls0@users.noreply.github.com>
2021-08-28 20:03:19 +1000

    Update lambda-route, initial boilerplate

---
+++ lambda-route/index.ts
@@ -0,0 +1,9 @@
+// import awssdk from "aws-sdk";
+
+exports.handler = async (event) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify("Hello from Lambda! route"),
+  };
+  return response;
+};
