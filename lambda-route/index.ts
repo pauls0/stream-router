13d2f534dd3411769e4864c511e051c40fa8072d
pauls0 <pauls0@users.noreply.github.com>
2021-08-29 23:00:52 +1000

    Update lambda-route, corrrectly formed response body

--- lambda-route/index.ts
+++ lambda-route/index.ts
@@ -1,9 +1,13 @@
 // import awssdk from "aws-sdk";
 
 exports.handler = async (event) => {
+  const Payload = {
+    perameters: JSON.stringify(event.queryStringParameters),
+    host: "hello-world",
+  };
   const response = {
     statusCode: 200,
-    body: JSON.stringify("Hello from Lambda! route"),
+    body: JSON.stringify(Payload),
   };
   return response;
 };
