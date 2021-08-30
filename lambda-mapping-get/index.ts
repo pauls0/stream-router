a664a2ee830cd0e84d5d82984c7677e6354c69b2
pauls0 <pauls0@users.noreply.github.com>
2021-08-30 20:10:10 +1000

    perf(mapping-get lambda): use get instead of query

--- lambda-mapping-get/index.ts
+++ lambda-mapping-get/index.ts
@@ -17,59 +17,57 @@ interface Payload {
 }
 
 async function getMapping(networkKey: string): Promise<Mapping> {
-  let mapping: Mapping;
+  // let mapping: Mapping;
 
   const params = {
     TableName: "stream-router-table-mappings",
-    ExpressionAttributeValues: {
-      ":network": networkKey,
+    Key: {
+      network: networkKey,
     },
-    KeyConditionExpression: "network = :network",
+    // ExpressionAttributeValues: {
+    //   ":network": networkKey,
+    // },
+    // KeyConditionExpression: "network = :network",
   };
 
-  await docClient
-    .query(params)
-    .promise()
-    .then((data) => {
-      if (data.Items) {
-        mapping = {
-          network: data.Items[0].network,
-          cdn: data.Items[0].cdn,
-        };
-      }
-    })
-    .catch((err) => {
-      console.error("ERROR:", JSON.stringify(err, null, 2));
-    });
-
-  return new Promise((resolve) => {
-    resolve(mapping);
-  });
+  let response = await docClient.get(params).promise();
+  return response.Item! as Mapping;
 }
 
 async function getPayload(): Promise<Payload> {
-  const mapping1 = await getMapping("1");
-  const mapping2 = await getMapping("2");
-  const mapping3 = await getMapping("3");
+  const mapping1 = getMapping("1");
+  const mapping2 = getMapping("2");
+  const mapping3 = getMapping("3");
 
   const payload = {
-    mappings: [mapping1, mapping2, mapping3],
+    mappings: await Promise.all([mapping1, mapping2, mapping3]),
   };
 
-  return new Promise((resolve) => {
-    resolve(payload);
-  });
+  return payload;
 }
 
 exports.handler = async (
   event: APIGatewayEvent
 ): Promise<APIGatewayProxyResult> => {
+  // await getMapping("1");
   const payload: Payload = await getPayload();
 
   const response = {
     statusCode: 200,
     body: JSON.stringify(payload),
   };
-
   return response;
 };
+
+// async function run() {
+//   // const payload: Payload = await getPayload();
+//   // const response = {
+//   //   statusCode: 200,
+//   //   body: JSON.stringify(payload),
+//   // };
+//   // console.log(response);
+
+//   let response = await getMapping("1");
+//   console.log(response);
+// }
+// run();
