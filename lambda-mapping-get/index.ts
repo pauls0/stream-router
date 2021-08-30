39b73e3a735e5537cc45c2d9f7ef14f498fe5d7d
pauls0 <pauls0@users.noreply.github.com>
2021-08-30 12:54:29 +1000

    feat(mapping-get lambda): get mapping from dynamoDB

--- lambda-mapping-get/index.ts
+++ lambda-mapping-get/index.ts
@@ -1,25 +1,75 @@
-// import awssdk from "aws-sdk";
+// import * as AWS from "aws-sdk";
+import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+import { config, DynamoDB } from "aws-sdk";
 
-interface Mappings {
+config.update({
+  region: "ap-southeast-2",
+});
+
+var docClient = new DynamoDB.DocumentClient();
+
+interface Mapping {
   network: number;
   cdn: string;
 }
 interface Payload {
-  mappings: Mappings[];
+  mappings: Mapping[];
 }
 
-const Payload: Payload = {
-  mappings: [
-    { network: 1, cdn: "" },
-    { network: 2, cdn: "" },
-    { network: 3, cdn: "" },
-  ],
-};
+async function getMapping(networkKey: string): Promise<Mapping> {
+  let mapping: Mapping;
 
-exports.handler = async () => {
+  const params = {
+    TableName: "stream-router-table-mappings",
+    ExpressionAttributeValues: {
+      ":network": networkKey,
+    },
+    KeyConditionExpression: "network = :network",
+  };
+
+  await docClient
+    .query(params)
+    .promise()
+    .then((data) => {
+      if (data.Items) {
+        mapping = {
+          network: data.Items[0].network,
+          cdn: data.Items[0].cdn,
+        };
+      }
+    })
+    .catch((err) => {
+      console.error("ERROR:", JSON.stringify(err, null, 2));
+    });
+
+  return new Promise((resolve) => {
+    resolve(mapping);
+  });
+}
+
+async function getPayload(): Promise<Payload> {
+  const mapping1 = await getMapping("1");
+  const mapping2 = await getMapping("2");
+  const mapping3 = await getMapping("3");
+
+  const payload = {
+    mappings: [mapping1, mapping2, mapping3],
+  };
+
+  return new Promise((resolve) => {
+    resolve(payload);
+  });
+}
+
+exports.handler = async (
+  event: APIGatewayEvent
+): Promise<APIGatewayProxyResult> => {
+  const payload: Payload = await getPayload();
+
   const response = {
     statusCode: 200,
-    body: JSON.stringify(Payload),
+    body: JSON.stringify(payload),
   };
+
   return response;
 };
