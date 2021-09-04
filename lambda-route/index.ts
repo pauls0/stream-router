bea57cda5e13dde56c1e096e47108ebea04098ec
pauls0 <pauls0@users.noreply.github.com>
2021-09-04 16:33:58 +1000

    feat(route lambda): convert ip to int

--- lambda-route/index.ts
+++ lambda-route/index.ts
@@ -1,13 +1,47 @@
-// import awssdk from "aws-sdk";
+// import * as AWS from "aws-sdk"; // import entire SDK
+import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+import { DynamoDB } from "aws-sdk";
 
-exports.handler = async (event) => {
-  const Payload = {
-    perameters: JSON.stringify(event.queryStringParameters),
+function iptoint(ip: String): number {
+  const octets = ip.split(".").map((octet) => parseInt(octet));
+  const ip_int =
+    octets[0] * 256 ** 3 + octets[1] * 256 ** 2 + octets[2] * 256 + octets[3];
+  return ip_int;
+}
+const ip: String = "104.28.18.197";
+const ip_int: number = iptoint(ip);
+console.log(ip_int);
+
+function inRange(range: String, ip: String): Boolean {
+  return false;
+}
+
+exports.handler = async (
+  event: APIGatewayEvent
+): Promise<APIGatewayProxyResult> => {
+  const payload: any = {
     host: "hello-world",
+    perameters: JSON.stringify(event.queryStringParameters),
   };
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(Payload),
-  };
-  return response;
+  if (
+    event.queryStringParameters == null ||
+    event.queryStringParameters == undefined
+  ) {
+    return {
+      statusCode: 400,
+      headers: {
+        "content-type": "application/json",
+      },
+      isBase64Encoded: false,
+      body: JSON.stringify({
+        message: "Error: queryStringParameter 'ip' not found",
+      }),
+    };
+  } else {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(payload),
+    };
+    return response;
+  }
 };
